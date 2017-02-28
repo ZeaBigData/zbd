3c60843a60df8ca5f470a3a405a5b0734900d6e2
yangjl0930@gmail.com <yangjl0930@gmail.com>
2017-02-28 15:53:13 -0800

    updates

--- profiling/1.accessions/1.A.1_PI_accessions.R
+++ profiling/1.accessions/1.A.1_PI_accessions.R
@@ -1,6 +1,17 @@
+### Jinliang Yang
+### 02-08-2017
+
+
 
 install.packages("xtable")
 library("xtable")
 d <- read.csv("data/export_2017_02_07_03_48_11.csv")
 
 print(xtable(d[1:10,]), type = "html")
+
+#install.packages("xtable")
+library("xtable")
+d <- read.csv("~/Documents/Github/ZeaBigData/data/export_2017_02_07_03_48_11.csv")
+
+print.xtable(xtable(d[1:1000,]), type = "html", ,sanitize.text.function=function(x){x})
+
