e3d76b157bc488cb52a3d284662b275638e7ee10
yangjl0930@gmail.com <yangjl0930@gmail.com>
2017-02-28 21:05:19 -0800

    updates

--- profiling/1.accessions/1.A.1_PI_accessions.R
+++ profiling/1.accessions/1.A.1_PI_accessions.R
@@ -11,7 +11,53 @@ print(xtable(d[1:10,]), type = "html")
 
 #install.packages("xtable")
 library("xtable")
-d <- read.csv("~/Documents/Github/ZeaBigData/data/export_2017_02_07_03_48_11.csv")
+library(data.table)
+d <- fread("~/Documents/Github/ZeaBigData/data/export_2017_02_07_03_48_11.csv", 
+           data.table=FALSE)
+names(d)
 
-print.xtable(xtable(d[1:1000,]), type = "html", ,sanitize.text.function=function(x){x})
 
+out <- as.data.frame(table(d$Origin))
+out <- subset(out, Var1 != "")
+
+out$c1<- gsub(" .*", "", out$Var1)
+out$c2 <- gsub("^[[:alpha:]]+\\s", "", out$Var1)
+
+out[out$Var1 == "United Kingdom England", ]$c1 <- "United Kingdom England"
+
+uq <- data.frame(table(out$c1))
+output <- data.frame()
+for(i in 1:nrow(uq)){
+    
+    if(uq$Freq[i] > 1){
+        sub <- subset(out, c1 %in% uq$Var1[i])
+        tem <- subset(sub, c1 == c2)
+        if(nrow(tem) == 0){
+            tem <- sub[1, ]
+            tem$c1 <- tem$Var1
+        }
+        if(nrow(tem) > 1){
+            tem <- tem[1, ]
+        }
+        tem$Freq <- sum(sub$Freq)
+    }else{
+        tem <- subset(out, c1 %in% uq$Var1[i])
+        tem$c1 <- tem$Var1
+    }
+    output <- rbind(output, tem)
+    message(sprintf("###>>> [ %sth ] country [ %s ]", i, uq$Var1[i]))
+}
+write.table(output, "data/grin_by_country.csv", sep=",", row.names=FALSE, quote=FALSE)
+
+sub1 <- subset(out, c1 != c2)
+sub1$Country <- paste(sub1$c1, sub1$c2)
+sub2 <- subset(out, c1 == c2)
+sub2$Country <- sub2$c1
+
+
+
+
+library("googleVis")
+Geo=gvisGeoChart(out, locationvar="Var1",  colorvar="Freq",
+                 options=list(projection="kavrayskiy-vii"))
+plot(Geo)
